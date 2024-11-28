bbfbdb72778e1c67fc4aa841846d4a0fce431fe8
SAIF EDDINE EL HAIMOUDI <haimoudi90@gmail.com>
2024-11-29 00:12:23 +0100

    Login Page - FrontEnd Finished!

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:agrimar/components/login_page.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
